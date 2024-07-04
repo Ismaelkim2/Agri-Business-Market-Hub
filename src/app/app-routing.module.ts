577ba86d28075cd2a322f0ec003b8cb7a10b76c1
ismael kimutai <ismaelkim2@gmail.com>
2024-07-04 16:24:34 +0300

    Update project configuration and dependencies
    - Modify angular.json to update project settings
    - Update package-lock.json to reflect dependency changes
    - Modify app-routing.module.ts for new routes
    - Update tsconfig.spec.json for configuration changes
    - Add yarn.lock to track yarn dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -49,7 +49,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
